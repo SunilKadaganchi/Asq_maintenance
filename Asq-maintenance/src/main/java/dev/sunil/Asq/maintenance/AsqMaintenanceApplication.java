bacd5ced8b72cfc0b3079c76cd016593603d3f20
SunilKadaganchi <sunilkadaganchi071@gmail.com>
2024-08-07 18:51:52 +0530

    ASQ maintenance initialization

---
+++ Asq-maintenance/src/main/java/dev/sunil/Asq/maintenance/AsqMaintenanceApplication.java
@@ -0,0 +1,13 @@
+package dev.sunil.Asq.maintenance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsqMaintenanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsqMaintenanceApplication.class, args);
+	}
+
+}
